1e4695a515ca4451dc55bc5f8f4946333c36e520
Greg Hurrell <greg.hurrell@liferay.com>
2019-01-28 13:54:05 +0100

    Update example JS in slides.mjs

--- content/slides.mjs
+++ content/slides.mjs
@@ -60,17 +60,17 @@ function text(content, attributes) {
 
 // Working with 400 x 300 viewbox.
 const skill = [
-  line(100, 0, 100, 200), // Y-axis.
-  polygon([[100, 0], [110, 10], [90, 10]], {fill: 'black'}),
-  text('value', {x: 10, y: 100}),
-  line(100, 200, 400, 200), // X-axis.
+  line(50, 0, 50, 200), // Y-axis.
+  polygon([[50, 0], [40, 10], [60, 10]], {fill: 'black'}),
+  text('value', {x: 0, y: 115, transform: 'rotate(270, 40, 100), translate(0, -20)'}),
+  line(50, 200, 400, 200), // X-axis.
   polygon([[400, 200], [390, 190], [390, 210]], {fill: 'black'}),
-  text('skill', {x: 225, y: 250, style: 'fill: green'}),
+  text('skill', {x: 200, y: 250, style: 'fill: green'}),
   curve([
-    [100, 200], // No skill, no value.
-    [125, 175],
-    [150, 25], // Increasing skill, increased value.
-    [200, 10],
+    [50, 200], // No skill, no value.
+    [75, 175],
+    [100, 25], // Increasing skill, increased value.
+    [150, 10],
     [375, 25], // Sustained plateau of high value.
     [400, 175] // Excessive skill, no value.
   ], {
@@ -82,16 +82,16 @@ render('#utility-chart-skill', skill);
 render('#utility-chart-size', [
   ...skill,
 
-  text('team/code size', {x: 135, y: 280, style: 'fill: red'}),
+  text('team/code size', {x: 120, y: 280, style: 'fill: red'}),
   curve([
-    [100, 175], // No size, not so much value.
-    [125, 170],
-    [150, 165],
-    [175, 160], // Increasing skill, increased value.
-    [200, 155],
-    [225, 150],
-    [250, 145],
-    [275, 140],
+    [50, 175], // No size, not so much value.
+    [75, 170],
+    [100, 165],
+    [125, 160], // Increasing skill, increased value.
+    [150, 155],
+    [175, 150],
+    [200, 145],
+    [225, 140],
     [375, 100],
     [400, 10] // Exponential take-off.
   ], {
